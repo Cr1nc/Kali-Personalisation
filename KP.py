9b07d3aee7261e5439b7d7e87c57c8db9da0dcae
Crinc <32513174+Cr1nc@users.noreply.github.com>
2019-05-18 19:30:58 +0100

    Update KP.py

--- KP.py
+++ KP.py
@@ -1,19 +1,53 @@
 import os
-os.system("apt update")
-os.system("apt upgrade")
-os.system("apt-get dist-upgrade")
-os.system("apt install terminator")
-os.system("apt install tmux")
-os.system("apt install git")
-os.system("apt install python-pip")
-os.system("apt install net-tools")
-os.system("gzip -d /usr/share/wordlists/rockyou.txt.gz")
-os.system("sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'")
-os.system("chsh -s /bin/zsh")
-###
+import colored
+
+#sets the stage
 os.system("clear")
-raw_input("Please press enter to continue:")
-print ("The Script will now install:")
+print("\033[1;31;40m \n")
+#updates and distribution upgrades system.
+wait_1 = raw_input("Press enter to begin:")
+os.system("clear")
+print "Would you like to perform an update and dist upgrade to the system?"
+update_upgrade = raw_input(":").lower()
+if update_upgrade.startswith('y'):
+        result = os.system("apt update && apt dist-upgrade")
+        print result
+if update_upgrade.startswith('n'):
+        print "Skipping to next step:"
+else:
+        print "Not recognised"
+        
+print("\033[1;31;40m \n")        
+#this will install multiple applications
+wait_2 = raw_input("Press enter to continue:")
+os.system("clear")
+print "Would you like to install the following applications:"
+print("\033[1;34;40m \n")
+print '''
+Tmux
+Git
+Pip
+Zsh
+Terminator
+Net-Tools
+'''
+print("\033[1;31;40m \n") 
+application_install = raw_input(":").lower()
+if application_install.startswith('y'):
+        result2 = os.system("apt install tmux git terminator python-pip net-tools zsh")
+        print result2
+if application_install.startswith('n'):
+        print "Skipping to next step:"
+else:
+        print "Not recognised"
+
+wait_3 = raw_input("Press enter to continue:")
+os.system("clear")
+print '''
+Would you like to install the following github scripts:
+They will be located at /root/Desktop/Tools
+'''
+print("\033[1;34;40m \n")
 print '''
 Cupp
 Mentalist
@@ -36,9 +70,10 @@ Red Snarf
 Impacket
 Null Linux
 '''
-###
+print("\033[1;31;40m \n") 
 print "And the following scripts/lists:"
 ###
+print("\033[1;34;40m \n") 
 print '''
 /var/www/html/...
 Linenum 
@@ -47,37 +82,16 @@ Linux Priv Checker
 /usr/share/wordlists/....
 SecLists Password Lists
 '''
-raw_input("Please press enter to continue:")
-###
-os.system("mkdir /Desktop/Tools")
-os.system("git clone https://github.com/Mebus/cupp.git /root/Desktop/Tools/Cupp")
-os.system("git clone https://github.com/sc0tfree/mentalist.git /root/Desktop/Tools/Mentalist")
-os.system("git clone https://github.com/D35m0nd142/LFISuite.git /root/Desktop/Tools/LFISuite")
-os.system("git clone https://github.com/leebaird/discover.git /root/Desktop/Tools/Discover")
-os.system("git clone https://github.com/thewhiteh4t/pwnedOrNot.git /root/Desktop/Tools/pwnedOrNot")
-os.system("git clone https://github.com/byt3bl33d3r/CrackMapExec /root/Desktop/Tools/CrackMapExec")
-os.system("git clone https://github.com/threat9/routersploit.git /root/Desktop/Tools/RouterSploit")
-os.system("git clone https://github.com/RUB-NDS/PRET.git /root/Desktop/Tools/PRET")
-os.system("git clone https://github.com/ShawnDEvans/smbmap.git /root/Desktop/Tools/SmbMap")
-os.system("git clone https://github.com/InteliSecureLabs/Linux_Exploit_Suggester.git /root/Desktop/Tools/Linux_Exploit_Suggester")
-os.system("git clone https://github.com/trustedsec/unicorn.git /root/Desktop/Tools/Unicorn")
-os.system("git clone https://github.com/EmpireProject/Empire.git /root/Desktop/Tools/Empire")
-os.system("git clone https://github.com/PowerShellMafia/PowerSploit.git /root/Desktop/Tools/PowerSploit")
-os.system("git clone https://github.com/s-rah/onionscan.git /root/Desktop/Tools/OnionScan")
-os.system("git clone https://github.com/m4ll0k/SMBrute.git /root/Desktop/Tools/SMBrute")
-os.system("git clone https://github.com/D35m0nd142/LFISuite.git /root/Desktop/Tools/LFISuite")
-os.system("git clone https://github.com/v3n0m-Scanner/V3n0M-Scanner.git /root/Desktop/Tools/V3n0M-Scanner")
-os.system("git clone https://github.com/nccgroup/redsnarf.git /root/Desktop/Tools/RedSnarf")
-os.system("git clone https://github.com/SecureAuthCorp/impacket.git /root/Desktop/Tools/Impacket")
-os.system("git clone https://github.com/m8r0wn/nullinux.git /root/Desktop/Tools/NullLinux")
-os.system("git clone https://github.com/rebootuser/LinEnum.git /var/www/html/LinEnum")
-os.system("git clone https://github.com/sleventyeleven/linuxprivchecker.git /var/www/html/linuxprivchecker")
-os.system("git clone https://github.com/danielmiessler/SecLists.git /usr/share/wordlists/SecLists")
-###
+gitscript_install = raw_input(":").lower()
+if gitscript_install.startswith('y'):
+        result3 = ["git clone https://github.com/Mebus/cupp.git /root/Desktop/Test/Cupp", "git clone https://github.com/sc0tfree/mentalist.git /root/Desktop/Test/Mentalist", "git clone https://github.com/D35m0nd142/LFISuite.git /root/Desktop/Test/LFISuite", "git clone https://github.com/leebaird/discover.git /root/Desktop/Tools/Discover", "git clone https://github.com/thewhiteh4t/pwnedOrNot.git /root/Desktop/Tools/pwnedOrNot", "git clone https://github.com/byt3bl33d3r/CrackMapExec /root/Desktop/Tools/CrackMapExec", "git clone https://github.com/threat9/routersploit.git /root/Desktop/Tools/RouterSploit", "git clone https://github.com/RUB-NDS/PRET.git /root/Desktop/Tools/PRET", "git clone https://github.com/ShawnDEvans/smbmap.git /root/Desktop/Tools/SmbMap", "git clone https://github.com/InteliSecureLabs/Linux_Exploit_Suggester.git /root/Desktop/Tools/Linux_Exploit_Suggester", "git clone https://github.com/trustedsec/unicorn.git /root/Desktop/Tools/Unicorn", "git clone https://github.com/EmpireProject/Empire.git /root/Desktop/Tools/Empire", "git clone https://github.com/PowerShellMafia/PowerSploit.git /root/Desktop/Tools/PowerSploit", "git clone https://github.com/s-rah/onionscan.git /root/Desktop/Tools/OnionScan", "git clone https://github.com/m4ll0k/SMBrute.git /root/Desktop/Tools/SMBrute", "git clone https://github.com/D35m0nd142/LFISuite.git /root/Desktop/Tools/LFISuite", "git clone https://github.com/v3n0m-Scanner/V3n0M-Scanner.git /root/Desktop/Tools/V3n0M-Scanner", "git clone https://github.com/nccgroup/redsnarf.git /root/Desktop/Tools/RedSnarf", "git clone https://github.com/SecureAuthCorp/impacket.git /root/Desktop/Tools/Impacket", "git clone https://github.com/m8r0wn/nullinux.git /root/Desktop/Tools/NullLinux", "git clone https://github.com/rebootuser/LinEnum.git /var/www/html/LinEnum", "git clone https://github.com/sleventyeleven/linuxprivchecker.git /var/www/html/linuxprivchecker", "git clone https://github.com/danielmiessler/SecLists.git /usr/share/wordlists/SecLists"]
+        for cmd in result3:
+            os.system(cmd)
+if gitscript_install.startswith('n'):
+        print "Skipping to next step:"
+else:
+        print "Not recognised"
+print("\033[1;31;40m \n") 
 
-raw_input("FINISHED      \o/          ")
-
-
-
-
-
+os.system("clear")
+print "FIN"
